e21bec19048317344763ffa6e23ee45e7cf7fe6b
Mohraiel <mohraielmatta@gmail.com>
2023-09-26 09:18:47 -0700

    moment.js library implemented it homejs

--- frontend/parkerfront/src/Home.js
+++ frontend/parkerfront/src/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+let moment = require('moment');
 function Home() {
+
+    const date = moment().format("MMM Do YY"); 
+    console.log(date);
   return (
     <div>
       <header>
         Welcome to Parker!
+        
       </header>
       <img src="/Parkerlogo.png" alt="Parker Logo" />
     </div>
